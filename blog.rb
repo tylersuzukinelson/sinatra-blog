da85dac2ddc35070fb488d3431ba3672f8dbdfdb
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-12 15:51:26 -0800

    Added database data to blog

--- blog.rb
+++ blog.rb
@@ -2,3 +2,30 @@ require "sinatra"
 require "data_mapper"
 
 use Rack::MethodOverride
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
+class Blog
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :title, String
+  property :body, Text
+end
+
+Blog.auto_upgrade!
+
+DataMapper.setup(:comments, "sqlite3://#{Dir.pwd}/comment.db")
+
+class Comment
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :blog_id, Integer
+  property :title, String
+  property :body, Text
+end
+
+DataMapper.repository(:comments) {
+  Comment.auto_upgrade!
+}
