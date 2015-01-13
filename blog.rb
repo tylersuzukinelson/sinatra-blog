3170996a93a36787c0b30cdbd96e60c73f86c19c
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-12 16:13:26 -0800

    Added outline for pages and to-do items for each page

--- blog.rb
+++ blog.rb
@@ -29,3 +29,41 @@ end
 DataMapper.repository(:comments) {
   Comment.auto_upgrade!
 }
+
+get "/" do
+  # TODO: Display all blog titles and bodies
+  # TODO: Limit body displayed to 200 chars; for those > 200 chars, tail with "..."
+  erb :home, layout: :blog_template
+end
+
+get "/create" do
+  # TODO: Add requirement for user authentication
+  # TODO: Add form to add a blog title and body
+  erb :create, layout: :blog_template
+end
+
+post "/view/:id" do |blog_id|
+  # TODO: Display title and body for given id
+  # TODO: Add form to add new comments
+  # TODO: Display existing comments ordered from newest to oldest
+  erb :view, layout: :blog_template
+end
+
+get "/admin" do
+  # TODO: Add requirement for user authentication
+  # TODO: Add links to delete or edit blog posts
+  erb :admin, layout: :blog_template
+end
+
+delete "/admin/delete/:id" do |blog_id|
+  # TODO: Add requirement for user authentication
+  # TODO: Delete the given blog post
+  # TODO: Delete the associated comments
+  redirect to("/admin")
+end
+
+patch "/admin/edit/:id" do |blog_id|
+  # TODO: Add requirement for user authentication
+  # TODO: allow the user to edit the title
+  erb :admin_edit, layout: :blog_template
+end
